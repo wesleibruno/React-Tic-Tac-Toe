29b4414dd383c2eb24ee5a8c148be2f8deeb5810
Weslei Bruno <weslei945@gmail.com>
2024-04-12 11:45:48 -0300

    winning combination

---
+++ src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditingClick() {
+    setIsEditing((isEditing) => !isEditing);
+  }
+
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  //   let btnCaption = "Edit";
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleNameChange}
+      />
+    );
+    // btnCaption = "Save";
+  }
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditingClick}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
+}
